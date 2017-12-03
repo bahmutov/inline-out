99411d8f0d0eb8cc19ec544f7f22269a44d21681
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-12-03 13:45:24 -0500

    chore: tsconfig

--- build/inline-out-spec.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-const _1 = require(".");
-describe('inline-out', () => {
-    it('write this test', () => {
-        console.assert(_1.default, 'should export something');
-    });
-});
